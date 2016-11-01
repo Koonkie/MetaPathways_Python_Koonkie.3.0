aa693e841a7238e41d0fb960c7d5670d59840844
dongjae <kishori82@gmail.com>
2016-11-01 13:19:38 -0700

    Fixed the paths for the regtests dir

--- run_regtests.sh
+++ run_regtests.sh
@@ -3,7 +3,7 @@
 
 #this tests the read counts and the biome format
 source MetaPathwaysrc
-python /home/sgeadmin/MetaPathways_Python_Koonkie.3.0/libs/python_scripts/MetaPathways_rpkm.py -c /home/sgeadmin/MetaPathways_Python_Koonkie.3.0/regtests/output/B1/preprocessed//B1.fasta  --rpkmExec /home/sgeadmin/MetaPathways_Python_Koonkie.3.0//executables/redhat/rpkm --rpkmdir regtests/input/reads -O /home/sgeadmin/MetaPathways_Python_Koonkie.3.0/regtests/output/B1/orf_prediction//B1.unannot.gff -o /home/sgeadmin/MetaPathways_Python_Koonkie.3.0/regtests/output/B1/results//rpkm/B1.orf_read_counts.txt -b /home/sgeadmin/MetaPathways_Python_Koonkie.3.0/regtests/output/B1/results//rpkm/B1.orf_read_counts.biom --sample_name  B1 --stats /home/sgeadmin/MetaPathways_Python_Koonkie.3.0/regtests/output/B1/results//rpkm/B1.orf_read_counts_stats.txt --bwaFolder /home/sgeadmin/MetaPathways_Python_Koonkie.3.0/regtests/output/B1/bwa/ --bwaExec /home/sgeadmin/MetaPathways_Python_Koonkie.3.0//executables/redhat/bwa
+python libs/python_scripts/MetaPathways_rpkm.py -c regtests/output/B1/preprocessed//B1.fasta  --rpkmExec executables/redhat/rpkm --rpkmdir regtests/input/reads -O regtests/output/B1/orf_prediction//B1.unannot.gff -o regtests/output/B1/results//rpkm/B1.orf_read_counts.txt -b regtests/output/B1/results//rpkm/B1.orf_read_counts.biom --sample_name  B1 --stats regtests/output/B1/results//rpkm/B1.orf_read_counts_stats.txt --bwaFolder regtests/output/B1/bwa/ --bwaExec executables/redhat/bwa
 
 
 FILE=regtests/output/B1/results/rpkm/B1.orf_read_counts.txt
